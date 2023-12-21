58c98e08c4c6a295d673eec26c99685b6e628287
Daniel Olsheski <dlo219@lehigh.edu>
2023-12-21 10:56:22 -0500

    Update the search box to be bigger and gain focus when the icon is clicked.

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -10,6 +10,18 @@ document.addEventListener("DOMContentLoaded", function () {
   }
   document.addEventListener('scroll', handleScroll);
 
+  function focusSearch() {
+    let searchIcon = document.querySelector('.search .search_btn');
+    let searchBox = document.querySelector('.main_nav .search_box');
+
+    if (searchIcon) {
+      searchIcon.addEventListener('click', function () {
+        searchBox.focus();
+      });
+    }
+  }
+
   // Initial check in case the page is already scrolled when the script is executed
   handleScroll();
+  focusSearch();
 });
